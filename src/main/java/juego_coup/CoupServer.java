147bc099ea5a4c58e35f6ba97cd6edf7958fb6b7
Alvarado2407 <l20441405@mochis.tecnm.mx>
2025-12-08 23:48:02 -0700

    metodo start

--- src/main/java/juego_coup/CoupServer.java
+++ src/main/java/juego_coup/CoupServer.java
@@ -8,6 +8,7 @@ import java.util.Iterator;
 import java.util.List;
 
 public class CoupServer {
+
     private static final int PORT = 12345;
     private static final int MAX_PLAYERS = 4;
     private final List<ClientHandler> handlers = new ArrayList();
@@ -23,5 +24,44 @@ public class CoupServer {
     public static void main(String[] args) {
         CoupServer server = new CoupServer();
         server.start();
+    }
+
+// Agrega el método start() a CoupServer.java
+    public void start() {
+        System.out.println("Coup Server is running on port 12345");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+
+            try {
+                while (this.playerCounter < 4) {
+                    System.out.println("Esperando al jugador #" + (this.playerCounter + 1) + "...");
+                    Socket clientSocket = serverSocket.accept();
+                    System.out.println("Cliente conectado desde: " + String.valueOf(clientSocket.getInetAddress()));
+                    // Nota: Asume que las clases Player, ClientHandler y Command existen.
+                    Player newPlayer = new Player("Player " + (this.playerCounter + 1), this.playerCounter);
+                    ClientHandler handler = new ClientHandler(clientSocket, this, newPlayer);
+                    this.handlers.add(handler);
+                    ++this.playerCounter;
+                    (new Thread(handler)).start();
+                }
+
+                System.out.println("¡Máximo de jugadores alcanzado! Iniciando la partida...");
+                this.initializeGame();
+            } catch (Throwable var6) {
+                try {
+                    serverSocket.close();
+                } catch (Throwable var5) {
+                    var6.addSuppressed(var5);
+                }
+
+                throw var6;
+            }
+
+            serverSocket.close();
+        } catch (IOException var7) {
+            System.err.println("Error en el servidor: " + var7.getMessage());
+            var7.printStackTrace();
+        }
     }
 }
