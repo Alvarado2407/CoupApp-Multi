43a3ce81a12031f65fb09bc669cf84933befcc4e
Alvarado2407 <l20441405@mochis.tecnm.mx>
2025-12-09 18:13:44 -0700

    cliente creado

--- src/main/java/juego_coup/CoupClient.java
+++ src/main/java/juego_coup/CoupClient.java
@@ -1,13 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package juego_coup;
 
-/**
- *
- * @author angel
- */
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class CoupClient {
-    
+    private static final String SERVER_ADDRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 12345;
+    private Socket socket;
+    private ObjectInputStream in;
+    private ObjectOutputStream out;
+    private Game localGame;
+    private int myPlayerNum = -1;
+    private final Scanner scanner;
+
+    public CoupClient() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public static void main(String[] args) {
+        CoupClient client = new CoupClient();
+        client.start();
+    }
 }
